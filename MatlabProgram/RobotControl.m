bff0db5d61493137fe3fab9f6963ab1daaea8231
kholodilinivan <79514433311@yandex.ru>
2023-09-05 14:18:15 +0500

    update

--- MatlabProgram/RobotControl.m
+++
@@ -1,3 +0,0 @@
-function RobotControl(TCP_Handle,w,s,a,d,speed,rotspeed)
-int = 1; % 0-img, 1-transform, 2-get position, 3-mode, 4-laser, 5-set position
-write(TCP_Handle,single([int,w,s,a,d,speed,rotspeed]));
